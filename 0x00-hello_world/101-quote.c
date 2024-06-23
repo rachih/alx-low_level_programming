2da009f24f539b0b752c00454a640ff49158fa77
rachih <rachih1996@gmail.com>
2024-06-23 11:00:45 -0700

    wdw

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <unistd.h>
 
+/**
+* main - A program that prints a specific message to the standard error
+* Return: 1 (error)
+*/
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char *message =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, 59);
 return (1);
+}
