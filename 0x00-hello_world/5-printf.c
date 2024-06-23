c6e1ff13b30c76bc578da43c66ed2e82298df292
rachih <rachih1996@gmail.com>
2024-06-23 10:09:08 -0700

    wdw

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with the printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("With proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
